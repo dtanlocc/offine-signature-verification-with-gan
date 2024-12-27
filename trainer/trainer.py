270844b33b03e4a96e84f990e1c40eb0de79ef11
Tan Loc <loc.dang.2209@gmail.com>
2024-12-27 20:02:47 +0700

    update trainer.py

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,12 +1,14 @@
 import torch
 import torch.optim as optim
-from utils.helpers import load_model, plot_tsne, plot_confusion_matrix, visualize_metrics_seaborn
+from utils.helpers import load_model, plot_confusion_matrix, visualize_metrics_seaborn, calculator_metric
 from utils.discriminator_loss import triplet_loss, contrastive_loss
 import torchvision.utils as vutils
-from sklearn.neighbors import KNeighborsClassifier
+import json
 from utils.meters import CatMeter, AverageMeter
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import os
+from tqdm.notebook import tqdm
+import torch.nn.functional as F
 
 
 class clf_signatureTrainer:
@@ -24,27 +26,43 @@ class clf_signatureTrainer:
         self.train_dataloader = train_dataloader
         self.test_dataloader = test_dataloader
         self.img_list = []
-        self.fixed_noise = torch.randn(64, opt.NZ, 1, 1, device=self.device)
+        self.fixed_noise = torch.randn(opt.BATCH_SIZE, opt.NZ, device=self.device)
         self.save_dir = opt.SAVE_DIR
         self.model_dir = opt.MODEL_DIR
         self.accuracies = []
         self.train_g_losses = []
         self.train_d_losses = []
+        self.results = {
+            'GAN': {
+                f'fold_{self.fold}': {
+                    'frr': [],
+                    'far': [],
+                    'acc': [],
+                    'loss_d': [],
+                    'loss_g': []
+                }
+            }
+        }
 
         self._init_model()
         self._init_criterion()
         self._init_optimizer()
+        self._init_scheduler()
 
     def _init_model(self):
         self.model_d, self.model_g = load_model(self.opt, self.device)
 
     def _init_optimizer(self):
         self.optimizer_g = optim.Adam(self.model_g.parameters(), lr=self.opt.LEARNING_RATE, betas=(0.5, 0.999))
-        self.optimizer_d = optim.Adam(self.model_d.parameters(), lr=self.opt.LEARNING_RATE, betas=(0.5, 0.999))
+        self.optimizer_d = optim.RMSprop(self.model_d.parameters(), lr=self.opt.LEARNING_RATE, eps=1e-8,
+                                         weight_decay=5e-4, momentum=0.9)
 
     def _init_criterion(self):
         self.triplet_loss = triplet_loss
         self.contrastive_loss = contrastive_loss
+
+    def _init_scheduler(self):
+        self.scheduler = optim.lr_scheduler.StepLR(self.optimizer_d, 5, 0.1)
 
     def train(self):
         best_accuracy = 0.0
@@ -54,82 +72,106 @@ class clf_signatureTrainer:
         for epoch in range(self.epochs):
             self.model_d.train()
             self.model_g.train()
-            train_dis_loss, train_gen_loss = self._train_one_epoch(epoch)
-            self.train_g_losses.append(train_dis_loss)
-            self.train_d_losses.append(train_gen_loss)
+            train_dis_loss, train_gen_loss, accuracy, cm, classification_report_dict = self._train_one_epoch(epoch)
+            self.train_g_losses.append(train_gen_loss)
+            self.train_d_losses.append(train_dis_loss)
+            self.accuracies.append(accuracy)
 
-            if (self.eval_step > 0) and (epoch + 1) % self.eval_step == 0 and epoch + 1 == self.epochs:
-                accuracy, _, _ = self.evaluate(epoch, tsne=False)
-                self.accuracies.append(accuracy)
-                if best_accuracy < accuracy:
-                    best_accuracy = accuracy
-                    best_epoch = epoch + 1
-                    self.save_model(epoch + 1)
-        accuracy, cm, classification_report_dict = self.evaluate(best_epoch, tsne=True)
-        visualize_metrics_seaborn(cm, self.save_dir)
-        plot_confusion_matrix(cm, self.save_dir)
+            self.results['GAN'][f'fold_{self.fold}']['loss_d'].append(train_dis_loss)
+            self.results['GAN'][f'fold_{self.fold}']['loss_g'].append(train_gen_loss)
+            if best_accuracy < accuracy:
+                best_accuracy = accuracy
+                best_epoch = epoch
+                self.save_model(epoch)
+        accuracy, cm, classification_report_dict = self.test(best_epoch, tsne=False, flag=True, roc=True)
+        far, frr, acc = calculator_metric(cm)
+        self.results['GAN'][f'fold_{self.fold}']['frr'].append(frr)
+        self.results['GAN'][f'fold_{self.fold}']['far'].append(far)
+        visualize_metrics_seaborn(classification_report_dict, self.save_dir + '/fold_' + str(self.fold))
+        plot_confusion_matrix(cm, class_names=['0', '1'], save_dir=self.save_dir + '/fold_' + str(self.fold))
         print(f'Best Accuracy: {best_accuracy:.4f}, epoch: {best_epoch}')
+        self.results['GAN'][f'fold_{self.fold}']['acc'].append(best_accuracy)
+        self.save_results(self.results)
+
+    def save_results(self, results):
+        os.makedirs(self.save_dir, exist_ok=True)  # Tạo thư mục nếu chưa tồn tại
+        save_path = os.path.join(self.save_dir, f'results_fold{self.fold}.json')
+        with open(save_path, 'w') as f:
+            json.dump(results, f, indent=4)
+        print(f"Saved results to {save_path}")
 
     def _train_one_epoch(self, epoch):
         self.d_loss_meter = AverageMeter()
         self.g_loss_meter = AverageMeter()
 
-        for i, sample in enumerate(self.train_dataloader):
+        progress_bar = tqdm(self.train_dataloader)
+        for i, sample in enumerate(progress_bar):
             # Dữ liệu đầu vào
-            self.user_id = sample['user'].to(self.device)
             self.image_1 = sample['image_1'].to(self.device)
             self.image_2 = sample['image_2'].to(self.device)
             self.label = sample['label'].to(self.device).float()
-            self.fake_labels = torch.zeros(self.image_1.size(0)).view(-1, 1).to(self.device)
-            self.real_labels = torch.ones(self.image_1.size(0)).view(-1, 1).to(self.device)
+            self.real_labels = torch.zeros(self.image_1.size(0)).view(-1, 1).to(self.device)
+            self.fake_labels = torch.ones(self.image_1.size(0)).view(-1, 1).to(self.device)
 
-            z = torch.randn(self.image_1.size(0), 100,1,1, device=self.device)
+            z = torch.randn(self.image_1.size(0), self.opt.NZ, 1, 1, device=self.device)
 
-            self.fake_image_2 = self.model_g(z, self.user_id)
+            self.fake_image_2 = self.model_g(z)
 
             # Train Discriminator và lưu loss
             d_loss = self.train_d()
-            self.d_loss_meter.update(d_loss, n=self.image_1.size(0))
+            self.d_loss_meter.update(d_loss)
 
             # Train Generator và lưu loss
             g_loss = self.train_g()
-            self.g_loss_meter.update(g_loss, n=self.image_1.size(0))
+            self.g_loss_meter.update(g_loss)
 
-            del self.user_id
-            del self.image_1
-            del self.fake_image_2
-            del self.image_2
+            progress_bar.set_description(
+                f"EPOCH [{epoch}/{self.epochs}]:Train dis loss: {d_loss:.4f}\tTrain gen loss: {g_loss:.4f}")
+
+            del self.image_1, self.fake_image_2, self.image_2, z
+            torch.cuda.empty_cache()
 
         # Check how the generator is doing by saving G's output on fixed_noise
         with torch.no_grad():
-            fake = self.model_g(self.fixed_noise, torch.full((64,), 44, dtype=torch.int).to(self.device)).detach().cpu()
+            fake = self.model_g(self.fixed_noise).to(self.device).detach().cpu()
         self.img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
         # Ghi nhận loss mean
         d_loss_avg, _ = self.d_loss_meter.value()
         g_loss_avg, _ = self.g_loss_meter.value()
 
-        # Ghi nhận loss mean
-        print(f"EPOCH {epoch + 1}:\tTrain dis loss: {d_loss_avg:.4f}\tTrain gen loss: {g_loss_avg:.4f}")
+        accuracy, cm, classification_report_dict = self.evaluate(epoch, tsne=False, flag=False, roc=True)
+        self.scheduler.step()
 
-        return d_loss_avg, g_loss_avg
+        # Ghi nhận loss mean
+        print(
+            f"EPOCH {epoch}:\tTrain dis loss: {d_loss_avg:.4f}\tTrain gen loss: {g_loss_avg:.4f}\tAccuracy: {accuracy:.4f}")
+        log_file = os.path.join(self.save_dir, f'train_log_fold{self.fold}.txt')
+        with open(log_file, 'a') as log:
+            log.write(
+                f"Epoch {epoch}:\tTrain dis loss: {d_loss_avg:.4f}\tTrain gen loss: {g_loss_avg:.4f}\tAccuracy: {accuracy:.4f}\n")
+        return d_loss_avg, g_loss_avg, accuracy, cm, classification_report_dict
 
     def train_d(self):
         # Zero gradients for Discriminator
         self.optimizer_d.zero_grad()
 
-        # Cosine similarity for real images
-        real_dist = self.model_d(self.image_1, self.image_2, self.user_id)
+        # feature_image from discriminator
+        image_features_1, image_features_2 = self.model_d(self.image_1, self.image_2)
+
+        # Calculator euclid
+        euclidean_distance_real = F.pairwise_distance(image_features_1, image_features_2, p=2)
 
         # Cosine similarity for fake images
-        fake_dist = self.model_d(self.image_1, self.fake_image_2.detach(), self.user_id)
+        image_features_1, image_features_2_fake = self.model_d(self.image_1, self.fake_image_2.detach())
+        euclidean_distance_fake = F.pairwise_distance(image_features_1, image_features_2_fake, p=2)
 
         # Compute the loss for real and fake images
-        d_loss_real = self.contrastive_loss(real_dist, self.label.view(-1, 1).to(self.device).float())
-        d_loss_fake = self.contrastive_loss(fake_dist, self.fake_labels)
+        d_loss_real = self.contrastive_loss(euclidean_distance_real, self.label.view(-1, 1).to(self.device).float())
+        d_loss_fake = self.contrastive_loss(euclidean_distance_fake, self.fake_labels)
 
         # Triplet loss between real and fake images
-        triplet_loss_value = self.triplet_loss(real_dist, fake_dist)
+        triplet_loss_value = self.triplet_loss(euclidean_distance_real, euclidean_distance_fake)
 
         # Total Discriminator loss
         d_loss = (d_loss_real + d_loss_fake) + triplet_loss_value
@@ -144,99 +186,107 @@ class clf_signatureTrainer:
         self.optimizer_g.zero_grad()
 
         # Get the discriminator's output for fake images
-        features_real = self.model_d.forward_once(self.image_1, self.user_id)
-        features_fake = self.model_d.forward_once(self.fake_image_2, self.user_id)
+        features_real, features_fake = self.model_d(self.image_1, self.fake_image_2)
 
         # Calculate Generator loss
         loss_fr = torch.mean((features_fake - features_real) ** 2)
 
-        # Adversarial Loss
-        dist_gen = self.model_d(self.image_1, self.fake_image_2, self.user_id)
+        # # Adversarial Loss
+        dist_gen = F.pairwise_distance(features_real, features_fake, p=2)
         loss_adv = contrastive_loss(dist_gen, self.real_labels)
 
         # Tổng Loss của Generator
-        g_loss = loss_adv + loss_fr
+        g_loss = loss_fr + loss_adv
 
         # Backpropagation and update the Generator
         g_loss.backward()
         self.optimizer_g.step()
+        del features_real, features_fake
+        torch.cuda.empty_cache()
 
         return g_loss.item()
 
     def _update_values(self, epoch):
         pass
 
-    def test(self, epoch, tsne):
+    def test(self, epoch, tsne, flag, roc):
         self.load_model(epoch)
         print("Testing ...")
-        self.evaluate(epoch, tsne)
+        return self.evaluate(epoch, tsne, flag, roc)
 
-    def evaluate(self, epoch, tsne=False):
-        print("START EVALUATION....")
+    def accuracy(self, distances, y, step=0.0001):
+        min_threshold_d = min(distances)
+        max_threshold_d = max(distances)
+
+        # step = (max_threshold_d-min_threshold_d)/1000
+        max_acc = 0
+        same_id = (y == 1)
+        d_optimal = float('inf')
+
+        # Duyệt qua các ngưỡng
+        thresholds = torch.arange(min_threshold_d, max_threshold_d + step, step)
+        for threshold_d in thresholds:
+            true_positive = (distances <= threshold_d) & same_id
+            true_negative = (distances > threshold_d) & (~same_id)
+
+            # Tính tỷ lệ đúng
+            true_positive_rate = true_positive.sum().float() / same_id.sum().float()
+            true_negative_rate = true_negative.sum().float() / (~same_id).sum().float()
+
+            # Tính accuracy
+            acc = 0.5 * (true_negative_rate + true_positive_rate)
+            if acc > max_acc:
+                max_acc = acc
+                d_optimal = threshold_d
+
+        return max_acc, d_optimal
+
+    def evaluate(self, epoch, tsne=False, flag=False, roc=True):
+        # print("START EVALUATION....")
         self.model_d.eval()
 
-        train_distances_meter, train_labels_meter = CatMeter(), CatMeter()
         test_distances_meter, test_labels_meter = CatMeter(), CatMeter()
         print("Calculating training distance ...")
-        with torch.no_grad():
-            for i, sample in enumerate(self.train_dataloader):
-                user_id = sample['user'].to(self.device)
-                image_1 = sample['image_1'].to(self.device)
-                image_2 = sample['image_2'].to(self.device)
-                label = sample['label'].to(self.device).float()
-
-                # Distance image_1, image_2 train_dataset
-                dist = self.model_d(image_1, image_2, user_id)
-                train_distances_meter.update(dist)
-                train_labels_meter.update(label)
 
         with torch.no_grad():
             for i, sample in enumerate(self.test_dataloader):
-                user_id = sample['user'].to(self.device)
                 image_1 = sample['image_1'].to(self.device)
                 image_2 = sample['image_2'].to(self.device)
                 label = sample['label'].to(self.device).float()
 
-                # Distance image_1, image_2 test_dataset
-                dist = self.model_d(image_1, image_2, user_id)
+                feature_1, feature_2 = self.model_d(image_1, image_2)
+                dist = F.pairwise_distance(feature_1, feature_2, p=2)
                 test_distances_meter.update(dist)
                 test_labels_meter.update(label)
 
-        # Convert kết quả sang numpy
-        train_distances = train_distances_meter.get_val_numpy()
-        train_labels = train_labels_meter.get_val_numpy()
+                del feature_1, feature_2
+                torch.cuda.empty_cache()
 
-        test_distances = test_distances_meter.get_val_numpy()
-        test_labels = test_labels_meter.get_val_numpy()
+        test_distances = test_distances_meter.get_val()
+        test_labels = test_labels_meter.get_val()
 
-        # Chuyển đổi thành mảng 2D
-        train_distances = train_distances.reshape(-1, 1)
-        test_distances = test_distances.reshape(-1, 1)
+        accuracy, thresold = self.accuracy(test_distances, test_labels)
 
-        # KNN Classifier
-        print("Evaluating with KNN....")
-        knn = KNeighborsClassifier(n_neighbors=2)
-        knn.fit(train_distances, train_labels)
-        predict_y = knn.predict(test_distances)
+        test_distances_cpu = test_distances_meter.get_val_numpy()
+        test_labels_cpu = test_labels_meter.get_val_numpy()
+        predict_y = (test_distances_cpu < thresold.numpy()).astype(int)
 
-        accuracy = accuracy_score(test_labels, predict_y)
-        print(f"Accuracy at epoch {epoch + 1}: {accuracy:.4f}")
+        accuracy = accuracy_score(test_labels_cpu, predict_y)
 
         # Tính confusion matrix
-        cm = confusion_matrix(test_labels, predict_y)
-        print("Confusion Matrix:")
-        print(cm)
+        cm = confusion_matrix(test_labels_cpu, predict_y)
 
-        class_report = classification_report(test_labels, predict_y, output_dict=True)
-        print("Classification Report:")
-        print(class_report)
+        class_report = classification_report(test_labels_cpu, predict_y, output_dict=True)
+        if flag:
+            print(f"Accuracy at epoch {epoch}: {accuracy:.4f}")
+            print("Confusion Matrix:")
+            print(cm)
+            print("Classification Report:")
+            print(class_report)
 
         if tsne:
-            plot_tsne(train_distances, train_labels, self.save_dir, epoch)
+            pass
 
-        del knn
-        del train_labels
-        del train_distances
         del test_labels
         del test_distances
 
@@ -244,15 +294,34 @@ class clf_signatureTrainer:
 
     def save_model(self, epoch):
         print("SAVING MODEL AT", str(epoch), " ...")
+        to_save = {
+            'model': self.model_d.state_dict(),
+            'scheduler': self.scheduler.state_dict(),
+            'optim': self.optimizer_d.state_dict(),
+        }
 
-        torch.save(self.model_g.state_dict(), os.path.join(self.model_dir, f"generator_fold{self.fold}_{str(epoch)}.pth"))
-        torch.save(self.model_d.state_dict(), os.path.join(self.model_dir,
-                                                           f'discriminator_fold{self.fold}_{str(epoch)}.pth'))
+        torch.save(to_save, os.path.join(self.model_dir,
+                                         f'discriminator_fold{self.fold}_{str(epoch)}.pth'))
+        torch.save(self.model_g.state_dict(),
+                   os.path.join(self.model_dir, f"generator_fold{self.fold}_{str(epoch)}.pth"))
 
     def load_model(self, epoch):
-        print("LOADING MODEL")
+        print("LOADING MODEL FROM", str(epoch), " ...")
+        checkpoint = torch.load(os.path.join(self.model_dir,
+                                             f'discriminator_fold{self.fold}_{str(epoch)}.pth'))
+
+        # Load model state
+        self.model_d.load_state_dict(checkpoint['model'])
+
+        # Load optimizer state
+        if 'optim' in checkpoint and self.optimizer_d is not None:
+            self.optimizer_d.load_state_dict(checkpoint['optim'])
+
+        # Load scheduler state
+        if 'scheduler' in checkpoint and self.scheduler is not None:
+            self.scheduler.load_state_dict(checkpoint['scheduler'])
 
         self.model_g.load_state_dict(torch.load(os.path.join(self.model_dir,
                                                              f'generator_fold{self.fold}_{str(epoch)}.pth')))
-        self.model_d.load_state_dict(torch.load(os.path.join(self.model_dir,
-                                                             f'discriminator_fold{self.fold}_{str(epoch)}.pth')))
+
+        print("MODEL LOADED SUCCESSFULLY")
