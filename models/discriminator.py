2fc5321a8d6e8f29acae76e00813a787122c91e8
Tan Loc <loc.dang.2209@gmail.com>
2025-01-15 23:48:52 +0700

    udpate discriminator

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,50 +1,56 @@
-import torch
 import torch.nn as nn
-from torchvision import models
 import torch.nn.functional as F
 
 
 class Discriminator(nn.Module):
-    def __init__(self, num_users, user_embedding_dim=128):
+    def __init__(self, output_dim=128):
         super(Discriminator, self).__init__()
 
-        # User Embedding layer
-        self.user_embedding = nn.Embedding(num_users, user_embedding_dim)
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.Dropout(0.3),
 
-        # ResNet50
-        resnet = models.resnet18(weights='ResNet18_Weights.DEFAULT')
-        self.resnet = nn.Sequential(*list(resnet.children())[:-1])  # Remove the final fully connected layer
+            nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.Dropout(0.3),
 
-        # Fully connected layers to process the combined features
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(384, 384, kernel_size=3, padding=1, groups=2),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+
+            nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+        )
+
         self.fc = nn.Sequential(
-            nn.Linear(2048 + user_embedding_dim, 1024),  # 2048 from ResNet + user_embedding_dim
-            nn.ReLU(),
-            nn.Linear(1024, 1)
+            nn.Linear(256 * 3 * 5, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(4096, output_dim)  # Kích thước đầu ra là output_dim
         )
 
-        self.up = nn.Sequential(
-            nn.Conv2d(1, 3, kernel_size=3, stride=1, padding=1),  # Convert 1 channel to 3 channels
-            nn.Upsample(size=(224, 224), mode='bilinear', align_corners=False),  # Resize image to 224x224
-        )
+    def forward_one(self, x):
+        x = self.cnn(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc(x)
+        return x
 
-    def forward_once(self, image, user_id):
-        image = self.up(image)
-        # Process image through ResNet50
-        image_features = self.resnet(image)
-        image_features = image_features.view(image_features.size(0), -1)  # Flatten to (batch_size, 2048)
-
-        # Get user embedding
-        user_embedding = self.user_embedding(user_id)
-
-        # Concatenate image features with user embedding
-        combined_features = torch.cat((image_features, user_embedding), dim=1)
-
-        return combined_features
-
-    def forward(self, image_1, image_2, user_id):
-        feature_image_1 = self.forward_once(image_1, user_id)
-        feature_image_2 = self.forward_once(image_2, user_id)
-
-        distance = F.pairwise_distance(feature_image_1, feature_image_2, p=2)
-        return distance
-
+    def forward(self, x1, x2):
+        out1 = self.forward_one(x1)
+        out2 = self.forward_one(x2)
+        euclidean_distance = F.pairwise_distance(out1, out2)
+        return euclidean_distance
