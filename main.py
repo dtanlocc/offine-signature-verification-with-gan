c253fcc604639d2d6a455c17b5270b6ff5665842
Tan Loc <loc.dang.2209@gmail.com>
2025-01-15 23:49:43 +0700

    udpate main

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from torchvision import transforms
-from utils.transform import Resize, RandomCrop, StandardizeImage
-from utils.create_dataloader import create_train_test_loaders
-from config.defaults import get_cfg_defaults
-from trainer.trainer import clf_signatureTrainer
+import json
+import os
 
+from torchvision import transforms
+
+from config.defaults import get_cfg_defaults
+from trainer.SignGanTrain import SignGanTrainer
+from utils.create_dataloader import create_train_test_loaders
+from utils.helpers import test_generator
 
 if '__main__' == __name__:
     opt = get_cfg_defaults()
     opt.freeze()
-    resize_transform = Resize(output_size=(64, 64))
-    random_crop_transform = RandomCrop(output_size=(64, 64))
 
     csv_files = ['D:/LVTN/data/CEDAR/data_fold_1.csv',
                  'D:/LVTN/data/CEDAR/data_fold_2.csv',
@@ -17,33 +18,39 @@ if '__main__' == __name__:
                  'D:/LVTN/data/CEDAR/data_fold_4.csv',
                  'D:/LVTN/data/CEDAR/data_fold_5.csv']
     path_root = 'D:/LVTN/data/CEDAR/CEDAR'
-    transform = transforms.Compose([resize_transform,
-                                    random_crop_transform,
-                                    transforms.ToTensor(),
-                                    StandardizeImage()])
-    results = {
-        'GAN': {'fold_0': {'far': [], 'frr': [], 'acc': [], 'optimal_threshold': [], 'loss_d': [], 'loss_g': []},
-                'fold_1': {'far': [], 'frr': [], 'acc': [], 'optimal_threshold': [], 'loss_d': [], 'loss_g': []},
-                'fold_2': {'far': [], 'frr': [], 'acc': [], 'optimal_threshold': [], 'loss_d': [], 'loss_g': []},
-                'fold_3': {'far': [], 'frr': [], 'acc': [], 'optimal_threshold': [], 'loss_d': [], 'loss_g': []},
-                'fold_4': {'far': [], 'frr': [], 'acc': [], 'optimal_threshold': [], 'loss_d': [], 'loss_g': []}, }
 
+    transform = transforms.Compose([
+        transforms.Resize((155, 220)),
+        transforms.CenterCrop((155, 220)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+    ])
+
+    results = {
+        'GAN': {f'fold_{i}': {'far': [], 'frr': [], 'acc': [], 'loss_d': [], 'loss_g': []} for i in range(5)}
     }
 
     for idx_fold_test in range(5):
         print('_______________________________________________________________________________')
         print(f"FOLD {idx_fold_test} START!!!")
-        train_loader, test_loader, num_users = create_train_test_loaders(csv_files,
-                                                                         path_root,
-                                                                         transform=transform,
-                                                                         batch_size=opt.BATCH_SIZE,
-                                                                         idx_fold_test=idx_fold_test)
+        train_loader, test_loader = create_train_test_loaders(csv_files,
+                                                              path_root,
+                                                              transform=transform,
+                                                              batch_size=opt.BATCH_SIZE,
+                                                              idx_fold_test=idx_fold_test)
+        trainer_SignGan = SignGanTrainer(opt, fold=str(idx_fold_test), train_dataloader=train_loader,
+                                         test_dataloader=test_loader)
+        trainer_SignGan.train()
+        # trainer_SignNet = SignNetTrainer(opt, fold=str(idx_fold_test), train_dataloader=train_loader, test_dataloader=test_loader)
+        # trainer_SignNet.train()
 
-        trainer = clf_signatureTrainer(opt, fold=str(idx_fold_test), train_dataloader=train_loader, test_dataloader=test_loader)
-        trainer.train()
+        results['GAN'][f'fold_{idx_fold_test}'] = trainer_SignGan.results['GAN'][f'fold_{idx_fold_test}']
+        # results['SIGNET'][f'fold_{idx_fold_test}'] = trainer_SignNet.results['SIGNET'][f'fold_{idx_fold_test}']
+        test_generator(trainer_SignGan.img_list, save_folder=f"{opt.SAVE_DIR_GAN}/fold_{idx_fold_test}")
 
+    # Save all results after training all folds
+    final_results_path = os.path.join(opt.SAVE_DIR_GAN, 'final_results.json')
+    with open(final_results_path, 'w') as f:
+        json.dump(results, f, indent=4)
 
-
-
-
-
+    print(f"Final results saved to {final_results_path}")
